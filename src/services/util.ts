344e79ba4ab36d3d5b61eb14cc0981013f9ec8bb
Rafa Peregrino <rbperegrino@gmail.com>
2023-07-07 08:01:09 -0300

    api url

--- src/services/util.ts
+++ src/services/util.ts
@@ -1,4 +1,4 @@
-const baseUrl = process.env.REACT_APP_BASE_URL || 'http://localhost:3001';
+const baseUrl = process.env.REACT_APP_BASE_URL || 'https://truenorth-api.azurewebsites.net';
 
 
 const getAuthHeader = () => {
